04662c1b088d596b2d959ddcbfaa8fb64f006e97
luckytoilet <bai.li.2005@gmail.com>
2017-10-17 00:49:21 -0400

    Add setup script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # bai-vimrc
+
 My personal vim configuration
+
+## Setup (Commandline)
+
+Run setup.sh and it should work
+
+## Setup (GUI)
+
+* Run setup.sh as before
+* Map Caps-Lock to Escape
+* Install Inconsolata font
+* Install Gruvbox and Solarized colorschemes
---
+++ setup.sh
@@ -0,0 +1,16 @@
+cd ~
+git clone https://github.com/luckytoilet/bai-vimrc
+mv bai-vimrc/.vimrc .vimrc
+mkdir .vim
+cd ~/.vim
+git clone https://github.com/jlanzarotta/bufexplorer
+mv bufexplorer/plugin/ .
+rm -rf bufexplorer/
+git clone https://github.com/scrooloose/nerdtree
+mv nerdtree/autoload/ .
+mv nerdtree/doc/ .
+mv nerdtree/lib/ .
+mv nerdtree/nerdtree_plugin/ .
+mv nerdtree/syntax/ .
+rsync -a nerdtree/plugin/ plugin/
+cd ~
