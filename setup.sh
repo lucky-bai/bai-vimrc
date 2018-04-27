0838decbeab0adfbc92ebce7e73be228c6546a3e
luckytoilet <bai.li.2005@gmail.com>
2018-04-27 11:18:13 -0400

    Add colorschemes to install script

--- setup.sh
+++ setup.sh
@@ -15,6 +15,14 @@ mv nerdtree/nerdtree_plugin/ .
 mv nerdtree/syntax/ .
 rsync -a nerdtree/plugin/ plugin/
 rm -rf nerdtree/
+git clone https://github.com/morhetz/gruvbox
+mv gruvbox/colors/ .
+rsync -a gruvbox/autoload/ autoload/
+rm -rf gruvbox
+git clone https://github.com/altercation/vim-colors-solarized
+rsync -a vim-colors-solarized/autoload/ autoload/
+rsync -a vim-colors-solarized/colors/ colors/
+rm -rf vim-colors-solarized
 cd ~
 rm -rf bai-vimrc/
 
