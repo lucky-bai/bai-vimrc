72bf909272bda7d2a57cfdd6b7131e61e375f394
luckytoilet <bai.li.2005@gmail.com>
2018-10-04 14:20:46 -0400

    Script to setup jupyter extensions

---
+++ setup-jupyter.sh
@@ -0,0 +1,6 @@
+mkdir -p $(jupyter --data-dir)/nbextensions
+cd $(jupyter --data-dir)/nbextensions
+git clone https://github.com/lambdalisue/jupyter-vim-binding vim_binding
+jupyter nbextension enable vim_binding/vim_binding
+git clone git://github.com/minrk/nbextension-scratchpad
+jupyter nbextension enable nbextension-scratchpad/main
